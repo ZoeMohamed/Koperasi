c8b676b55992831cffeab304df4c5d30c0b95e4f
ZoeMohamed <zoemohamed31@gmail.com>
2022-11-08 05:49:13 +0700

    f

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:koperasi/screens/dashboard.dart';
 import 'package:koperasi/screens/login.dart';
 import 'package:koperasi/screens/transcreen.dart';
+import 'package:sizer/sizer.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -17,14 +18,16 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        LoginPage.routeName: (context) => const LoginPage(),
-      },
-      home: const Scaffold(
-        body: Bottomnav(),
-      ),
-    );
+    return Sizer(builder: (cntxt, orientation, builder) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          LoginPage.routeName: (context) => const LoginPage(),
+        },
+        home: const Scaffold(
+          body: LoginPage(),
+        ),
+      );
+    });
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: prefer_const_constructors
-
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 
 class LoginPage extends StatefulWidget {
   // Route for Login
@@ -16,6 +17,10 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  var width;
+  var height;
+
   @override
   void initState() {
     WidgetsBinding.instance!.addPostFrameCallback((_) {
@@ -23,180 +28,99 @@ class _LoginPageState extends State<LoginPage> {
           systemNavigationBarColor: Colors.white,
           systemNavigationBarIconBrightness: Brightness.dark));
     });
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Image.asset(
-          "assets/build.png",
-        ),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-          body: SingleChildScrollView(
-            child: Column(children: [
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: height,
+          width: width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
               SizedBox(
-                height: 405,
+                height: height * 0.15,
               ),
               Center(
-                child: Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20)),
+                child: SizedBox(
+                  width: width * 0.88,
+                  child: Image.asset(
+                    "assets/images/static/asset_login.png",
+                    width: width,
                   ),
-                  height: 438,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 32),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            "Log in.",
-                            style: TextStyle(
-                                fontFamily: "Nunito-Extra-Bold",
-                                fontSize: 40,
-                                color: Color(0xFF25396F),
-                                fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.08,
+              ),
+              Text(
+                "Login",
+                textAlign: TextAlign.left,
+                style: GoogleFonts.montserrat(
+                  textStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 35,
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.08,
+              ),
+              Center(
+                child: Form(
+                  key: _formKey,
+                  child: Expanded(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: width * 0.85,
+                          height: height * 0.28,
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Field Username Wajib diisi!";
+                              }
+                              return null;
+                            },
+                            onSaved: (val) {},
+                            decoration: InputDecoration(
+                              errorStyle: TextStyle(color: Colors.orange),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 0.9, color: Color(0xFF7E7E7E)),
+                              ),
+                              hintText: "Username",
+                              hintStyle: TextStyle(
+                                  color: Color(0xFF7E7E7E).withOpacity(0.7),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: 'Roboto-Medium'),
+                              prefixIcon: Icon(
+                                FluentIcons.person_16_regular,
+                                color: Color(0xFF7E7E7E),
+                              ),
+                              alignLabelWithHint: true,
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      Form(
-                          child: Column(
-                        children: [
-                          SizedBox(
-                            width: 340,
-                            child: TextFormField(
-                              controller: _nameController,
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return "Field Username Wajib diisi!";
-                                }
-                                return null;
-                              },
-                              onSaved: (val) {},
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                prefixIcon: Icon(FluentIcons.person_12_regular),
-                                errorStyle: TextStyle(height: 0.9),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      width: 0.9, color: Color(0xFF7E7E7E)),
-                                ),
-                                hintText: "Username",
-                                hintStyle: TextStyle(
-                                    color: Color(0xFF7E7E7E).withOpacity(0.7),
-                                    fontWeight: FontWeight.w500,
-                                    fontFamily: 'Roboto-Medium'),
-                                alignLabelWithHint: true,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 45,
-                          ),
-                          SizedBox(
-                            width: 340,
-                            child: TextFormField(
-                              controller: _passwordController,
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return "Field Password Wajib diisi!";
-                                }
-                                return null;
-                              },
-                              decoration: InputDecoration(
-                                prefixIcon:
-                                    Icon(FluentIcons.shield_keyhole_16_regular),
-                                border: OutlineInputBorder(),
-                                hintText: "Password",
-                                errorStyle: TextStyle(height: 0.9),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      width: 0.9, color: Color(0xFF7E7E7E)),
-                                ),
-                                hintStyle: TextStyle(
-                                    color: Color(0xFF7E7E7E).withOpacity(0.7),
-                                    fontWeight: FontWeight.w500,
-                                    fontFamily: 'Roboto-Medium'),
-                                alignLabelWithHint: true,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 40,
-                          ),
-                          Material(
-                            child: InkWell(
-                              highlightColor: Colors.green.withOpacity(0.4),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: Color(0xFF435EBE)),
-                                height: 40,
-                                width: 300,
-                                child: Center(
-                                    child: Text(
-                                  "Login",
-                                  style: TextStyle(color: Colors.white),
-                                )),
-                              ),
-                              onTap: () async {},
-                            ),
-                          )
-                        ],
-                      )),
-                      SizedBox(
-                        height: 7,
-                      ),
-                      Text(
-                        "Or",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      SizedBox(
-                        height: 7,
-                      ),
-                      Material(
-                        child: InkWell(
-                          highlightColor: Colors.green.withOpacity(0.4),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                color: Color(0xFFEA4335)),
-                            height: 40,
-                            width: 300,
-                            child: Center(
-                                child: Text(
-                              "Continue With Google",
-                              style: TextStyle(color: Colors.white),
-                            )),
-                          ),
-                          onTap: () async {},
-                        ),
-                      )
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               )
-            ]),
+            ],
           ),
-        )
-      ],
+        ),
+      ),
     );
   }
- Widget _buildForm(){
 
-  return Text("data");
+  Widget _buildForm() {
+    return Text("data");
   }
 }
